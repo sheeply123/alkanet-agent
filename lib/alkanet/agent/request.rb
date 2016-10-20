657b52d846aef15cb224983b3134da95424c9989
Kosaka Yuki <ykosaka@asl.cs.ritsumei.ac.jp>
2016-10-20 18:34:23 +0900

    fix api path

--- lib/alkanet/agent/request.rb
+++ lib/alkanet/agent/request.rb
@@ -15,27 +15,27 @@ module Alkanet
       end
 
       def fetch_collector_info
-        @api_clinet.get('/internal/api/collectors/oneself.json')
+        @api_clinet.get('/api/collectors/oneself.json')
       end
 
       def fetch_job_info(id)
-        @api_clinet.get("/internal/api/jobs/#{id}.json")
+        @api_clinet.get("/api/jobs/#{id}.json")
       end
 
       def update_job_info(id, params)
-        @api_clinet.put("/internal/api/jobs/#{id}.json", params)
+        @api_clinet.put("/api/jobs/#{id}.json", params)
       end
 
       def upload_tracelog(id, tracelog)
         tracelog_io = Faraday::UploadIO.new(tracelog, 'binary/octet-stream')
-        @api_clinet.post("/internal/api/jobs/#{id}/tracelog", tracelog: tracelog_io) do |req|
+        @api_clinet.post("/api/jobs/#{id}/tracelog", tracelog: tracelog_io) do |req|
           req.options.timeout = 0
         end
       end
 
       def upload_report(id, tracelog)
         tracelog_io = Faraday::UploadIO.new(tracelog, 'binary/octet-stream')
-        @api_clinet.post("/internal/api/jobs/#{id}/report", report: tracelog_io) do |req|
+        @api_clinet.post("/api/jobs/#{id}/report", report: tracelog_io) do |req|
           req.options.timeout = 0
         end
       end
